fee10749341b1a36da408f3b833865800289ea2d
bubbybumble <bubbybumble@gmail.com>
2024-08-21 14:32:45 -0400

    imported routing outlet

--- frontend/FRSDemoFrontend/src/app/app.routes.ts
+++ frontend/FRSDemoFrontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
+
 import { Routes } from '@angular/router';
+import { DatagridComponent } from './components/datagrid/datagrid.component';
+
 
 export const routes: Routes = [
     {
-       
+       path: 'datagrid',
+       component: DatagridComponent
     }
 ];
